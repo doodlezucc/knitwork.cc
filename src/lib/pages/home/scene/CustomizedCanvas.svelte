0c2ce3e468fff5790e569b89c3d9729c30ca1110
doodlezucc <electerminator@gmail.com>
2026-04-19 01:28:19 +0200

    Use configured WebGLRenderer

---
+++ src/lib/pages/home/scene/CustomizedCanvas.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core';
+	import type { Snippet } from 'svelte';
+	import { NeutralToneMapping, PCFShadowMap, WebGLRenderer } from 'three';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
+
+<Canvas
+	toneMapping={NeutralToneMapping}
+	shadows={PCFShadowMap}
+	createRenderer={(canvas) => {
+		const renderer = new WebGLRenderer({
+			canvas,
+			powerPreference: 'default',
+			antialias: true,
+			alpha: true
+		});
+		renderer.debug.checkShaderErrors = false;
+		return renderer;
+	}}
+>
+	{@render children()}
+</Canvas>
